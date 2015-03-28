2ac5e111872df384e4e783b251beba37266f87a5
Bas Rustenburg <bas.rustenburg@choderalab.org>
2015-03-28 17:02:58 -0400

    Simplify ingredients.rb, split off salads. Also added a menus directory and the legourmet menu

--- Ingredients.rb
+++ Ingredients.rb
@@ -1,34 +1,15 @@
+require 'money'
+
 class Ingredient
-    attr_accessor :price, :taste, :date
-    def initialize(price,taste, date)
+    attr_accessor :name, :price
+    def initialize(name ,price)
+        @name = name
         @price = price
-        @taste = taste
-        @date = date
     end
 end
 
-class Recipe
-    attr_accessor :leaf, :toppings, :dressings
-    def initialize(leaf, toppings, dressings)
-        @leaf = leaf
-        @toppings = toppings
-        @dressings = dressings        
-    end
-    def score
-        price = 0.0
-        @toppings.each { |top| price += top.price }
-        if @dressings.length >= 1
-            price += 0.50 * (@dressings - 1)
-        end
-        taste = 0.0
-        @toppings.each { |top| taste += top.taste }
-        @dressings.each do |dress|
-            taste += dress.taste 
-        end
-        
-        x *price + y * taste + z 
-        
-        return price
-    end
 
-Sonya = Ingredient.new(0.75, "mehh")
+if __FILE__ == $0
+    puts "One day, this will contain ingredients."
+
+end
