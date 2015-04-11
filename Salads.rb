8a7a476cd67f4200795763c6d527bf0f280fd7a7
Bas Rustenburg <bas.rustenburg@choderalab.org>
2015-04-11 16:01:06 -0400

    Added documentation to Salad

--- Salads.rb
+++ Salads.rb
@@ -1,34 +1,79 @@
 #! /usr/bin/env ruby
 
+
+# A container type class for composing a salad
 class Salad
-    attr_accessor :ingredients, :vegetarian
+
+    # @ingredients is an Array with ingredients
+    attr_accessor :ingredients
+    # @vegetarian is a boolean tag for whether the salad is vegetarian or not
+    attr_accessor :vegetarian
+    # @dressing is an Array of dressings
+    attr_accessor :dressing
+
+    # ==== Parameters
+    #
+    # * +base_price+ - the base price for the salad
+
     def initialize (base_price)
         @base = base_price
         @ingredients = Array.new
         @vegetarian = true
         @dressing = Array.new
     end
+
+    # Add an ingredient to salad
+    #
+    # ==== Parameters
+    #
+    # * +ingredient+ - an Ingredient object
+    # * +meat+ - boolean tag, is ingredient a meat
     def add_ingredient(ingredient, meat=false)
         if meat
             @vegetarian = false
         end
         @ingredients.push(ingredient)
     end
+
+    # Add a dressing to salad
+    #
+    # ==== Parameters
+    #
+    # * +dressing+ - an Ingredient coresponding to a dressing
     def add_dressing(dressing)
         @dressing.push(dressing)
     end
+
+    # Add an Array of Ingredients to salad
+    #
+    # ==== Parameters
+    #
+    # * +ingredients+ - an Array of Ingredient objects
+    # * +meat+ - boolean tag, is ingredient a meat
     def add_ingredients(ingredient_list, meat=false)
         if meat
             @vegetarian = false
         end
         ingredient_list.each {|ingredient| self.add_ingredient(ingredient)}
     end
+
+    # Calculate the price of the salad
+    #
+    # ==== Returns
+    # A float containing the total price
+    #
     def price
         total = @base
         @ingredients.each { |ingredient| total += ingredient.price }
         @dressing.each_with_index { |dressing,index| index > 0 ? total += dressing.price : next }
         return total
     end
+
+    # Returns a representation of the salad
+    #
+    # ==== Returns
+    #
+    # A string representing all the ingredients of the salad and the prices
     def inspect
         # Representation of Salad object
         repres = "Salad_ID = #{self.object_id}\n"
