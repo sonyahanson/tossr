a17bff09b876c7a8aa6c1a2c16d16c0fdb7e303e
Bas Rustenburg <bas.rustenburg@choderalab.org>
2015-05-02 14:40:08 -0400

    Now read menu using menu reader, instead of LG.rb

--- Menu_reader.rb
+++ Menu_reader.rb
@@ -1,4 +1,7 @@
 require 'yaml'
+require './Salads.rb'
+
+include Salads
 
 # Read a yaml file containing a menu
 def read_yml_menu(filename)
@@ -8,10 +11,22 @@ def read_yml_menu(filename)
     meats = settings[:ingredients][:meats]
     vegetables = settings[:ingredients][:vegetables]
     dressings = settings[:ingredients][:dressings]
-    leaf = settings[:ingredients][:leaves]
+    leaf = settings[:ingredients][:leaf]
 
-    ingredients = { :meats => meats, :vegetables => vegetables, :dressings => dressings, :leaf => leaf}
+    ingredients = { :meats => hash_to_ingredient(meats), :vegetables => hash_to_ingredient(vegetables), :dressings => hash_to_ingredient(dressings), :leaf => hash_to_ingredient(leaf)}
     sizes = settings[:sizes]
-    
-    return Ingredients, Sizes
+    return ingredients, sizes
+end
+
+# Convert a hash of ingredients with metadata to a list of Ingredient objects
+def hash_to_ingredient(dict)
+    ingredients = Array.new
+
+    # Each ingredient has name as key, and metadata as a dictionary.
+    # At this point, we just extract the price, but we can expand this later
+    puts dict.inspect
+    dict.each do |name,ingr|
+        ingredients.push(Salads::Ingredient.new(name, ingr["price"]))
+    end
+    return ingredients
 end
--- Toss.rb
+++ Toss.rb
@@ -1,16 +1,16 @@
 require './Salads.rb'
-require './LeGourmet.rb'
-
-include LeGourmet
+require './Menu_reader.rb'
 include Salads
 
-base_price = LeGourmet::Sizes[:small]
+ingredients, sizes = read_yml_menu("legourmet.yml")
+
+base_price = sizes[:small]
 number_of_ingredients = 3
 
 salad = Salads::Salad.new(base_price)
-salad.add_ingredient(LeGourmet::Ingredients[:leaf].sample)
-salad.add_dressing(LeGourmet::Ingredients[:dressings].sample)
-salad.add_ingredients(LeGourmet::Ingredients[:vegetables].sample(number_of_ingredients))
-salad.add_ingredient(LeGourmet::Ingredients[:meats].sample, meat=true)
+salad.add_ingredient(ingredients[:leaf].sample)
+salad.add_dressing(ingredients[:dressings].sample)
+salad.add_ingredients(ingredients[:vegetables].sample(number_of_ingredients))
+salad.add_ingredient(ingredients[:meats].sample, meat=true)
 
 puts salad.inspect
