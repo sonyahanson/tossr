29657a91df7de1de1d4599a32771ebac1cf21867
Bas Rustenburg <bas.rustenburg@choderalab.org>
2016-03-27 15:08:32 -0400

    Generate xml output containing ingredients.

--- recipes/recipes.wikia.com/parse_wiki.rb
+++ recipes/recipes.wikia.com/parse_wiki.rb
@@ -3,4 +3,33 @@ require 'nokogiri'
 @doc.remove_namespaces!
 pages = @doc.xpath("//page")
 
-puts pages[2]
+def parse_wikia_recipe(text)
+    ingredients = []
+    
+    text.each_line do |line|
+        if line.start_with?("*")
+            ingredients.push(line)
+        end
+    end
+    if ingredients.length == 0
+        puts text
+    end
+    return ingredients
+    
+end
+
+builder = Nokogiri::XML::Builder.new do |xml|
+  xml.root {
+      
+    xml.salads {
+        
+        pages.each_with_index  do |page,pi|
+            xml.salad {
+                xml.ingredients = parse_wikia_recipe(page.xpath("//text")[pi].to_s)
+            }
+        end
+      }
+  }
+end
+File.open('tossr.xml', 'w') { |file| file.write(builder.to_xml) }
+    
